13fe1aaec1ad60a8c418f7d309573c77d076c1d0
Jackson Rudnick <jacksonrudnick32@gmail.com>
2024-02-08 09:32:02 -0600

    Added comments

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import sys
 
 #generate large prime numbers
 def prime_generation():
+	#change to 1024 or something larger at some point
 	num_of_bits = 256
 	
+	#generate a random large number
 	random_prime = random.randrange(2**(num_of_bits-1)+1, 2**(num_of_bits)-1)
 	
 	return random_prime
@@ -14,24 +16,27 @@ def prime_generation():
 def prime_test(n):
 	k=5
 	
-	if n == 1 or n == 4:
+	#initial check
+	if n == 1:
 		return False
-	if n == 2 or n == 3:
-		return True
 
+	#fermat test
 	for i in range(k):
 		a = random.randint(2, n-2)
 		if pow(a, n-1, n) != 1:
 			return False
 	
+	#number is prime
 	return True
 
 #create public key using phi
 def get_public_key(phi):
 	pq = (p-1)*(q-1)
 	
+	#get a random number
 	random_num = random.randrange(2, pq)
 
+	#check number with euclid's gcd
 	while math.gcd(random_num, pq) != 1:
 		random_num = random.randrange(2, pq)
 
@@ -66,13 +71,15 @@ def decrypt_message(ciphertext, private_key, pq):
 def encode(s):
 	return int.from_bytes(bytes(s, 'utf-8'), byteorder=sys.byteorder)
 
-#decode message into a number
+#decode message into a string
 def decode(n):
     return (n.to_bytes(255, byteorder=sys.byteorder)).decode('utf-8', "ignore")
 
+#generate a signature as a private user
 def generate_signature(name, private_key, n):
 	return pow(encode(name), private_key, n)
 
+#verify a signature as a public user
 def check_signature(signature, public_key, n):
 	return decode(pow(int(signature), public_key, n))
 
@@ -91,6 +98,7 @@ n = None
 
 #main loop
 while True:
+	#options
 	print("Please select an option: \n	1. Public User\n	2. Private User\n	3. Generate Keys\n	4. Exit\nEnter your choice: ")
 
 	user_exit = int(input())
@@ -118,6 +126,7 @@ while True:
 			print(encrypt_message(m, public_key, n))
 		#Authenticating a signature
 		elif user_operation == 2:
+			#check if user has a defined n for their public key
 			if n == None:
 				print("Please enter n for your public key:")
 				try:
@@ -130,6 +139,7 @@ while True:
 			print("Please input the signature:")
 			name = input()
 			
+			#check the signature
 			print(check_signature(name, public_key, n))
 
 	#Private Key User
@@ -144,11 +154,13 @@ while True:
 				print("Key Error")
 				quit()
 		
+		#options
 		print("Please select an option: \n	1. Decrypt A Message\n	2. Generate A Digital Signature\nEnter your choice: ")
 		user_operation = int(input())
 
 		#Dencrypting a message
 		if user_operation == 1:
+			#get the cipher text
 			print("What is the message?")
 			try:
 				ciphertext = int(input())
@@ -159,6 +171,7 @@ while True:
 			print(decrypt_message(ciphertext, private_key, n))
 		#Creating a signature
 		elif user_operation == 2:
+			#check if they have an n defined for their private key
 			if n == None:
 				print("Please enter n for your private key:")
 				try:
@@ -171,6 +184,7 @@ while True:
 			print("Please input your signature:")
 			name = input()
 			
+			#generating the signature
 			print(generate_signature(name, private_key, n))
 
 	#User wants to generate new keys
@@ -191,6 +205,7 @@ while True:
 
 		private_key = get_private_key(phi, public_key)
 
+		#display the keys
 		print("Public Key :: ", public_key, "\n")
 		print("Private Key :: ", private_key, "\n")
 		print("N :: ", n, "\n")
